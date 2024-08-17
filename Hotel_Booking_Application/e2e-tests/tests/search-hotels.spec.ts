325dde4c64e725fe0ee64db4994b35b9634c2eb6
Rahul Morabiya <132494312+Rahul-Morabiya@users.noreply.github.com>
2024-08-17 10:59:22 +0530

    Add files via upload

---
+++ Hotel_Booking_Application/e2e-tests/tests/search-hotels.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect } from "@playwright/test";
+import path from "path";
+const UI_URL = "http://localhost:5173/";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(UI_URL);
+  await page.getByRole("link", { name: "Sign In" }).click();
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+  await page.locator("[name=email]").fill("2@2.com");
+  await page.locator("[name=password]").fill("password123");
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.getByText("Sign In Successful!")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+});
+
+test("Should show hotel search results", async ({ page }) => {
+  await page.goto(UI_URL);
+  await page.getByPlaceholder("Where are you going?").fill("Dublin");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await expect(page.getByText("Hotels found in Dublin")).toBeVisible();
+  await expect(page.getByText("Dublin Getaways")).toBeVisible();
+});
+
+test("should show hotel detail", async ({ page }) => {
+  await page.getByPlaceholder("Where are you going?").fill("Dublin");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Dublin Getaways").click();
+  await expect(page).toHaveURL(/detail/);
+  await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
+});
+
+test("should book hotel", async ({ page }) => {
+  await page.getByPlaceholder("Where are you going?").fill("Dublin");
+
+  const date=new Date();
+  date.setDate(date.getDate()+3);
+  const formattedDate=date.toISOString().split("T")[0];
+
+
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Dublin Getaways").click();
+  await page.getByPlaceholder("Check-out Date").fill(formattedDate);
+
+  await page.getByRole("button",{name:"Book now"}).click();
+
+  await expect(page.getByText("Total Cost: Rs 357.00")).toBeVisible();
+  
+  await page.getByRole("button",{name:"Confirm Booking"}).click();
+  await expect(page.getByText("Booking Saved!")).toBeVisible();  
+
+  await page.getByRole("link",{name:"My Bookings"}).click();
+  await expect(page.getByText("Dublin Getaways")).toBeVisible();
+});
